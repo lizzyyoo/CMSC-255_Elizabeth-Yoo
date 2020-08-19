32ef72fe5dcaa68b3adf7eddffec2a076ba95c63
lizzyyoo <69876562+lizzyyoo@users.noreply.github.com>
2020-08-19 12:15:34 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/************************************************************************
+* Lab 1  
+*************************************************************************
+* Elizabeth Yoo
+* August 19, 2020
+* CMSC 255 004
+************************************************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Elizabeth Yoo");
+		System.out.println("Lab 1");
+		System.out.println("Introduction to the Course for Mac");
+		System.out.println("August 19, 2020");
+		System.out.println("CMSC 255 004");
+		System.out.println();
+	}
+}
